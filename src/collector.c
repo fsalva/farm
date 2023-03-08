4a68b2db4479d6a859801f969dd0c6d010423189
Francesco <francescosalvatori99@gmail.com>
2023-03-08 21:33:41 +0100

    Aggiunto (tra le altre cose) XOPEN_SOURCE 700 per esporre definizioni non-standard per qualche motivo non esposte (man 7 feature_test_macros)

--- src/collector.c
+++ src/collector.c
@@ -4,7 +4,7 @@
 #include <pthread.h>
 #include <signal.h>
 #include <errno.h>
-
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
--- src/masterworker.c
+++ src/masterworker.c
@@ -1,7 +1,10 @@
+#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/un.h>
 #include <sys/wait.h>
 #include <sys/types.h>

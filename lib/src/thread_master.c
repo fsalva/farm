f00df84f74884377782faa13c12ad786d366ac42
Francesco <francescosalvatori99@gmail.com>
2023-03-09 20:06:27 +0100

    Spostata la recursive_filewalk su MasterWorker.c

--- lib/src/thread_master.c
+++ lib/src/thread_master.c
@@ -13,8 +13,6 @@
 #include <time.h>
 #include <sys/time.h>
 #include <sys/syscall.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include <pthread.h>
 #include <errno.h>
 
@@ -84,58 +82,4 @@ void *  master_function (void * arg) {
     return NULL;
 }
 
-/**
- * @brief Scorre ricorsivamente tutti i files e tutte le directory (ed il loro contenuto)
- *  A partire dalla cartella dirname. 
- *  Inserisce i nomi dei file trovati nella lista l.
- * 
- * @param dirname 
- * @param l 
- */
-void recursive_file_walk_insert(char * dirname, list * l) {
-
-    DIR *dir = {0};
-    struct dirent *entry = {0};
-    struct stat info = {0};
-
-    char path[MAX_MSG_SIZE];
-
-    // Terminazione della ricorsione: 
-    if (!(dir = opendir(dirname)))
-        return;
-
-    // Finché accedo ad una cartella ed è tutto ancora attivo:
-    // (Potrei ricevere il segnale di terminazione
-    //  mentre scorro la lista di files): 
-
-    while ((entry = readdir(dir)) != NULL && master_running) {
-        
-        // Accodo il percorso ottenuto fino ad ora: 
-        snprintf(path, sizeof(path), "%s/%s", dirname, entry->d_name);
-        
-        if(stat(path, &info) != 1) {
-            
-            // Se 'path' è una directory: 
-            if (S_ISDIR(info.st_mode)) {
-
-                // Ignoro la directory corrente e quella passata: 
-                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
-
-                // Controllo dentro la directory: 
-                recursive_file_walk_insert(path, l);
-
-            } 
-            // Se 'path' è un file regolare: 
-            else if(S_ISREG(info.st_mode))
-            {
-                // Lo inserisco nella lista di files da elaborare.
-                list_insert(l, path);
-            }
-        }
-        else { // Gestione errore
-            perror("stat() :");
-        }
-    }
-    closedir(dir);
-}
 
--- src/masterworker.c
+++ src/masterworker.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <dirent.h>
+
 #include <sys/un.h>
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -25,6 +27,9 @@
 int             pid_child = -1;
 int             fatal_error = 0;
 volatile sig_atomic_t    master_running = 1;
+
+void recursive_file_walk_insert(char * dirname, list * l);
+
 
 // Invia al processo Collector un SIGUSR2 (Trigger stampa istantanea)
 void handler_sigusr1(int signum) {
@@ -240,4 +245,59 @@ void cleanup() {
     while ((wpid = waitpid(pid_child, &status, 0)) > 0)
     {
     }
+}
+
+/**
+ * @brief Scorre ricorsivamente tutti i files e tutte le directory (ed il loro contenuto)
+ *  A partire dalla cartella dirname. 
+ *  Inserisce i nomi dei file trovati nella lista l.
+ * 
+ * @param dirname 
+ * @param l 
+ */
+void recursive_file_walk_insert(char * dirname, list * l) {
+
+    DIR *dir = {0};
+    struct dirent *entry = {0};
+    struct stat info = {0};
+
+    char path[MAX_MSG_SIZE];
+
+    // Terminazione della ricorsione: 
+    if (!(dir = opendir(dirname)))
+        return;
+
+    // Finché accedo ad una cartella ed è tutto ancora attivo:
+    // (Potrei ricevere il segnale di terminazione
+    //  mentre scorro la lista di files): 
+
+    while ((entry = readdir(dir)) != NULL && master_running) {
+        
+        // Accodo il percorso ottenuto fino ad ora: 
+        snprintf(path, sizeof(path), "%s/%s", dirname, entry->d_name);
+        
+        if(stat(path, &info) != 1) {
+            
+            // Se 'path' è una directory: 
+            if (S_ISDIR(info.st_mode)) {
+
+                // Ignoro la directory corrente e quella passata: 
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+
+                // Controllo dentro la directory: 
+                recursive_file_walk_insert(path, l);
+
+            } 
+            // Se 'path' è un file regolare: 
+            else if(S_ISREG(info.st_mode))
+            {
+                // Lo inserisco nella lista di files da elaborare.
+                list_insert(l, path);
+            }
+        }
+        else { // Gestione errore
+            perror("stat() :");
+        }
+    }
+    closedir(dir);
 }
